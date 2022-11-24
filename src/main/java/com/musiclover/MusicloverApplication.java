130cf04e6ceefb35b86d38ca0fb6a4299bbfd0d4
ElenaS1981 <elenasidyakina@gmail.com>
2022-11-24 11:43:01 -0800

    Project set up with dependencies/maven and added User class

---
+++ src/main/java/com/musiclover/MusicloverApplication.java
@@ -0,0 +1,13 @@
+package com.musiclover.musiclover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicloverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicloverApplication.class, args);
+	}
+
+}
